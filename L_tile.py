03dc41f68cea78aed5a52d9ed2f0b28d818df27e
Mozoby <brthornbury@csupomona.edu>
2014-10-15 09:41:21 -0700

    L-Tile implementation successful:

--- L_tile.py
+++ L_tile.py
@@ -9,39 +9,50 @@ Author: Bryan Thornbury
 #	exclude ::= (tuple) (x,y) coords of excluded tile 
 # Returns: (boolean) whether or not the tiling configuration can be completed
 
-def ltile(k, exclude):
+def ltile(k, ex):
 	colors = [i for i in range(4**k, 0, -1)]
-	return ltileRecurse([[None for x in  range(2**k)] for y in range(2**k)], k, exclude, [0,0], 2**k, colors)
+	return ltileRecurse([[None for x in  range(2**k)] for y in range(2**k)], k, ex, [0,0], 2**k, colors)
 
-
+#prints the array neatly
 def printArr(tp):
+	tp = tp[:]
+	tp.reverse()
 	for row in tp:
 		print '\t'.join(map(str, row))
 
+#verifies every number appears exactly 3 times
+def verifyArr(arr):
+	count = dict()
+
+	for row in arr:
+		for value in row:
+			if value is not None:
+				if value in count:
+					count[value] += 1
+				else:
+					count[value] = 1
+	for key in count.keys():
+		if count[key] != 3: 
+			return False
+	return True
+
 def ltileRecurse(arr, k, exclude, bl, size, colors):
-	#print size
 	if size == 2:
 		color = colors.pop()
 		for rIndex, row in enumerate(arr[bl[1]:bl[1]+size]):
-			#print rIndex, bl, row[bl[0]:bl[0] + size]
 			for i in range(len(row[bl[0]:bl[0] + size])):
 				if(exclude[0] != (bl[0] + i) or exclude[1] != (bl[1] + rIndex)):
 					row[bl[0] + i] = color
-				#else:
-					#print 'excluded', exclude
-		#printArr(arr)
 		return arr
-		#return [ [color if (exclude[0] - bl[0] != j or exclude[1] - bl[1] != i) else None for j in range(2) ] for i in range(2)]
 	else:
 		#gather sub states
 		for dy in range(2):
 			for dx in range(2):
 				nBl = (bl[0] + (dx*size//2), bl[1] + (dy*size//2))
-				tExclude = exclude
+				tExclude = exclude[:]
 				#Define the empty node
-				if( 0 < exclude[0] - nBl[0] < size//2 and  0 < exclude[1] - nBl[1] < size//2):
-					print "don't change"
-					tExclude = exclude #don't change it
+				if( 0 <= exclude[0] - nBl[0] < size//2 and  0 <= exclude[1] - nBl[1] < size//2):
+					tExclude = exclude[:] #don't change it
 				else:
 					#change excluded to the inner
 					if(dx == 0):
@@ -53,48 +64,26 @@ def ltileRecurse(arr, k, exclude, bl, size, colors):
 						tExclude[1] = nBl[1] + (size//2) - 1
 					else:
 						tExclude[1] = nBl[1]
-				#print "recurse", dx, dy
-				#print tExclude, nBl
-				ltileRecurse(arr,k, tExclude, nBl, size//2, colors)
-		#print arr
 
-		#place extra tile
+				ltileRecurse(arr,k, tExclude, nBl, size//2, colors)
+
+		#Place extra tile
 		color = colors.pop()
 		for rIndex, row in enumerate(arr[bl[1]:bl[1]+size]):
 			for i in range(len(row[bl[0]:bl[0] + size])):
-				if (exclude[0] != (bl[0] + i) or exclude[1] != (bl[1] + rIndex)) and row[i] is None:
+				if (exclude[0] != (bl[0] + i) or exclude[1] != (bl[1] + rIndex)) and row[bl[0] + i] is None:
 					row[bl[0] + i] = color
-
 		return arr
 
-		# #Construct This 2D Array from subArrays
-		
-		# printArr(arr)
-		# #tState.reverse()
-		# for index, subArr in enumerate(tState):
-		# 	tBl = ((index % 2) * size//2, (index//2) *size//2 )
-		# 	#print tBl
-		# 	for y,row in enumerate(subArr):
-		# 		for x, value in enumerate(row):
-		# 			#print "(",tBl[1] + y, tBl[0] + x,")", value
-		# 			arr[tBl[1] + y][tBl[0] + x] = value
-		# printArr(arr)
-		# #Fill in the missing pieces
-		# tColor = colors.pop()
-		# #print tColor
-		# for y, row in enumerate(arr):
-		# 	for x, value in enumerate(row):
-		# 		if(value is None and ((exclude[0] - bl[0]) == x and (exclude[1] - bl[1]) == y)):
-		# 			#fill with color
-		# 			arr[y][x] = tColor
-		# 		else:
-		# 			arr[y][x] = value
-
-		# return arr
-
-tp = ltile(3, [7,7])
-printArr(tp)
-#tp.reverse()
+good = True
+for k in range(1,6):
+	print k
+	for i in xrange(2**k):
+		for j in xrange(2**k):
+			tp = ltile(k, [i,j])
+			#printArr(tp)
+			good = good and verifyArr(tp)
+print "All Cases Verified:", good
 
 
 

1fa13ba79bd75cd2ac6003b82e616b0601f0d808
Mozoby <brthornbury@csupomona.edu>
2014-10-15 09:02:49 -0700

    L-tile much much closer

--- L_tile.py
+++ L_tile.py
@@ -11,22 +11,28 @@ Author: Bryan Thornbury
 
 def ltile(k, exclude):
 	colors = [i for i in range(4**k, 0, -1)]
-	print colors
-	return ltileRecurse(k, exclude, [0,0], 2**k, colors)
+	return ltileRecurse([[None for x in  range(2**k)] for y in range(2**k)], k, exclude, [0,0], 2**k, colors)
 
 
 def printArr(tp):
 	for row in tp:
-		print ' '.join(map(str, row))
+		print '\t'.join(map(str, row))
 
-def ltileRecurse(k, exclude, bl, size, colors):
+def ltileRecurse(arr, k, exclude, bl, size, colors):
 	#print size
 	if size == 2:
 		color = colors.pop()
-		#print color
-		return [ [color if (exclude[0] - bl[0] != j or exclude[1] - bl[1] != i) else None for j in range(2) ] for i in range(2)]
+		for rIndex, row in enumerate(arr[bl[1]:bl[1]+size]):
+			#print rIndex, bl, row[bl[0]:bl[0] + size]
+			for i in range(len(row[bl[0]:bl[0] + size])):
+				if(exclude[0] != (bl[0] + i) or exclude[1] != (bl[1] + rIndex)):
+					row[bl[0] + i] = color
+				#else:
+					#print 'excluded', exclude
+		#printArr(arr)
+		return arr
+		#return [ [color if (exclude[0] - bl[0] != j or exclude[1] - bl[1] != i) else None for j in range(2) ] for i in range(2)]
 	else:
-		tState = []
 		#gather sub states
 		for dy in range(2):
 			for dx in range(2):
@@ -49,34 +55,42 @@ def ltileRecurse(k, exclude, bl, size, colors):
 						tExclude[1] = nBl[1]
 				#print "recurse", dx, dy
 				#print tExclude, nBl
-				subArr = ltileRecurse(k, tExclude, nBl, size//2, colors)
-				printArr(subArr)
-				tState.append(subArr)
+				ltileRecurse(arr,k, tExclude, nBl, size//2, colors)
+		#print arr
 
-		#Construct This 2D Array from subArrays
-		arr = [[None] * size]*size
-		printArr(arr)
-		#tState.reverse()
-		for index, subArr in enumerate(tState):
-			tBl = ((index % 2) * size//2, (index//2) *size//2 )
-			#print tBl
-			for y,row in enumerate(subArr):
-				for x, value in enumerate(row):
-					#print "(",tBl[1] + y, tBl[0] + x,")", value
-					arr[tBl[1] + y][tBl[0] + x] = value
-		printArr(arr)
-		#Fill in the missing pieces
-		tColor = colors.pop()
-		#print tColor
-		for y, row in enumerate(arr):
-			for x, value in enumerate(row):
-				if(value is None and ((exclude[0] - bl[0]) == x and (exclude[1] - bl[1]) == y)):
-					#fill with color
-					arr[y][x] = tColor
-				else:
-					arr[y][x] = value
+		#place extra tile
+		color = colors.pop()
+		for rIndex, row in enumerate(arr[bl[1]:bl[1]+size]):
+			for i in range(len(row[bl[0]:bl[0] + size])):
+				if (exclude[0] != (bl[0] + i) or exclude[1] != (bl[1] + rIndex)) and row[i] is None:
+					row[bl[0] + i] = color
 
 		return arr
+
+		# #Construct This 2D Array from subArrays
+		
+		# printArr(arr)
+		# #tState.reverse()
+		# for index, subArr in enumerate(tState):
+		# 	tBl = ((index % 2) * size//2, (index//2) *size//2 )
+		# 	#print tBl
+		# 	for y,row in enumerate(subArr):
+		# 		for x, value in enumerate(row):
+		# 			#print "(",tBl[1] + y, tBl[0] + x,")", value
+		# 			arr[tBl[1] + y][tBl[0] + x] = value
+		# printArr(arr)
+		# #Fill in the missing pieces
+		# tColor = colors.pop()
+		# #print tColor
+		# for y, row in enumerate(arr):
+		# 	for x, value in enumerate(row):
+		# 		if(value is None and ((exclude[0] - bl[0]) == x and (exclude[1] - bl[1]) == y)):
+		# 			#fill with color
+		# 			arr[y][x] = tColor
+		# 		else:
+		# 			arr[y][x] = value
+
+		# return arr
 
 tp = ltile(3, [7,7])
 printArr(tp)

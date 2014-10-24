81e87f376a98296b4bad64391a39f920aab78a3a
Mozoby <brthornbury@csupomona.edu>
2014-10-24 11:06:15 -0700

    Dynamic Programming: Optimal Binary Search Tree

--- project1.py
+++ project1.py
@@ -10,7 +10,7 @@ def memoize(f):
 	def call(*args, **kwargs):
 		if('clear' in kwargs and kwargs['clear']): 
 			recorded.clear()
-			return
+
 		if(args[1:] in recorded):
 			return recorded[args[1:]]
 		else:
@@ -19,48 +19,28 @@ def memoize(f):
 	return call
 
 def computeAverageProb(node, depth = 1):
-	if(node is None): 
-		return 0
-	return (node[0][1] * depth) + reduce(lambda x,y: x+y, [computeAverageProb(n, depth+1) for n in node[1:3]])
-
-
+	return (lambda:(node[0][1] * depth) + reduce(lambda x,y: x+y, [computeAverageProb(n, depth+1) for n in node[1:3]]), lambda:0)[node is None]()
 
 @memoize
-def optimalBinaryTreeRecurse(arr,lb, rb):
-	if(lb == rb): 
-		return tuple([arr[lb], None, None])
+def optimalBinaryTree(arr,lb, rb):
+	if(lb == rb): return (arr[lb], None, None)
 
 	minTree = None
 	minCost = float("inf")
 
 	for root in range(lb,rb+1):
 		#compute min right subtree
-		right = None
-		rightCost = float("inf")
-		for k in range(root+1, rb+1): #i+1 to rightBound
-			rc = optimalBinaryTreeRecurse(arr, root+1, rb) #right candidate
-			rcCost = computeAverageProb(rc)
-			if( rcCost < rightCost):
-				right = rc 
-				rightCost = rcCost
+		right = optimalBinaryTree(arr, root+1, rb)
 
 		#compute min left subtree
-		left = None
-		leftCost = float("inf")
-		for k in range(lb, root): #lb to root - 1
-			lc = optimalBinaryTreeRecurse(arr, lb,root-1) #left candidate
-			lcCost = computeAverageProb(lc)
-			if( lcCost < leftCost):
-				left = lc 
-				leftCost = lcCost
+		left = optimalBinaryTree(arr, lb,root-1)
 
 		candidateCost = computeAverageProb((arr[root],left,right))
 		if(candidateCost < minCost):
 			minTree = (arr[root],left,right)
 			minCost = candidateCost
 
-
-	return tuple([arr[root], left, right])
+	return minTree
 
 def stringifyTree(node):
 	if(node is None): return ""
@@ -72,12 +52,13 @@ def stringifyTree(node):
 	return node[0][0] + '(' + left + ',' + right + ')'
 
 
-tests = [ [('Don', 3/8.0), ('Isabelle', 3/8.0), ('Ralph', 1/8.0), ('Wally', 1/8.0)],
-			[('A', 0.1), ('B', 0.2), ('C', 0.4), ('D', 0.3)],
-			[('A',1/7.0),('B',1/7.0),('C',1/7.0),('D',1/7.0),('E',1/7.0),('F',1/7.0),('G',1/7.0)]
- ]
+tests = [[('Don', 3/8.0), ('Isabelle', 3/8.0), ('Ralph', 1/8.0), ('Wally', 1/8.0)],
+		[('A', 0.1), ('B', 0.2), ('C', 0.4), ('D', 0.3)],
+		[('A',1/7.0),('B',1/7.0),('C',1/7.0),('D',1/7.0),('E',1/7.0),('F',1/7.0),('G',1/7.0)]]
 
-for t in tests:
-	optimalBinaryTreeRecurse(clear=True)
-	result = optimalBinaryTreeRecurse(t,0,len(t)-1)
-	print  computeAverageProb(result),  stringifyTree(result)
+print '\n'.join([' => '.join([str(computeAverageProb(tree)), stringifyTree(tree)]) for tree in [optimalBinaryTree(t,0,len(t)-1, clear=True) for t in tests]])
+# for t in tests:
+# 	#Clear memoization
+# 	optimalBinaryTree(clear=True)
+# 	result = optimalBinaryTree(t,0,len(t)-1)
+# 	print  computeAverageProb(result),  stringifyTree(result)
